07e864574a6c42bad102b32824d8402512c0bbdd
kmonguu <skdus8009@gmail.com>
2022-06-23 00:05:39 +0900

    fix: 영화 사진 랜덤하게 노출되도록 수정 및 입력값 노출되도록 수정

--- src/Poster/index.tsx
+++ src/Poster/index.tsx
@@ -1,21 +1,40 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
+import { useState } from 'react'
 import { useGetMovies } from '../hooks/movie'
+import { jsonLocalStorage } from '../lib/storage'
 
 interface PosterProps {
   randomNumber: number
-  value: any
 }
 
-function Poster({ randomNumber, value }: PosterProps) {
+function Poster({ randomNumber }: PosterProps) {
   const { movies } = useGetMovies()
+  // const [favorit, setFavorit] = useState([])
+  // const [mainPoster, setMainPoster] = useState(<img src={movies[0]} />)
+
+  // const handleFavoritButton = () => {
+  //   const nextFavorites = [...favorit, mainPoster]
+  //   setFavorit(nextFavorites)
+  //   jsonLocalStorage.setItem('favorit', nextFavorites)
+  // }
+
+  const movieTitle = jsonLocalStorage.getItem('movie-title')
 
   if (!movies) return <div>로딩중입니다...</div>
 
   return (
     <div css={wrapperCss}>
       <img src={movies[randomNumber]} alt="영화 포스터" />
-      <p>{value}</p>
+      <p>{movieTitle}</p>
+
+      {/* <ul>
+        {favorit.map((movie) => (
+          <li>
+            <img src={movies[randomNumber]} key={movie} />
+          </li>
+        ))}
+      </ul> */}
     </div>
   )
 }

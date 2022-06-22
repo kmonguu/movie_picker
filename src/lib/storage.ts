4b6a2d1b2817fd8cf4f537d3fcc09ea623f99c99
kmonguu <skdus8009@gmail.com>
2022-06-22 23:36:39 +0900

    fix: storage 코드 별도 파일로 분리

---
+++ src/lib/storage.ts
@@ -0,0 +1,10 @@
+export const jsonLocalStorage = {
+  setItem: (key: string, value: any) => {
+    localStorage.setItem(key, JSON.stringify(value))
+  },
+  getItem: (key: string) => {
+    const item = localStorage.getItem(key)
+    if (!item) return
+    return JSON.parse(item)
+  },
+}
